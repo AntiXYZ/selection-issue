ccb71e19438359d0c524a3eabc05f0772f8976bf
mikhail chistiakov <chistyakovmikhail.mc@gmail.com>
2020-08-28 11:23:16 -0400

    created

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import WebView from "react-native-webview";
+
+const Drawer = createDrawerNavigator();
+
+const WebViewScreen = (props) => {
+  return (
+    <WebView
+      source={{
+        html:
+          "<div style='font-size: 40'>Dolore occaecat ex deserunt est officia incididunt excepteur elit laborum. Enim cillum voluptate amet tempor aliquip labore adipisicing eiusmod culpa commodo anim eu magna. Ad reprehenderit nulla laborum laborum Lorem veniam culpa pariatur consectetur incididunt veniam cillum minim. Excepteur veniam commodo labore laborum aute pariatur excepteur fugiat enim ullamco ut deserunt ea. Anim laboris anim anim Lorem aliquip aute. Dolor elit do aliqua consectetur laboris officia Lorem Lorem ut id officia dolor tempor.</div>>",
+      }}
+    />
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen
+          key={"webview"}
+          name={"webview"}
+          component={WebViewScreen}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
